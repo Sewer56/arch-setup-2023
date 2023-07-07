8a9ebd366be1d695e97ce4093e4b8b369b1b3d0b
sewer56lol <sewer56lol@googlemail.com>
2023-07-07 01:15:17 +0100

    Added: Auto-fix bookmarks on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Copy over Files
 chmod -R 755 ./.config/
 cp -R ./.config/* $HOME/.config
 cp -R ./Pictures/* $HOME/Pictures
 
+# Setup zsh
 chsh -s $(which zsh)
 cp -R ./.zshenv $HOME/.zshenv
+
+# Fix bookmarks
+sed -i "s|/home/your_username|/home/$USER|g" ~/.config/gtk-3.0/bookmarks
