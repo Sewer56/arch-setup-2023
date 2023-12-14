cd2cdce3e635b89596a73f5f2b4b0c3bbb731132
sewer56 <sewer56lol@googlemail.com>
2023-12-14 20:04:11 +0000

    NAS: Wait for internet connectivity for mounting

---
+++ .config/hypr/scripts/start_nas.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+{
+    # Wait for network connectivity
+    while ! ping -c 1 -W 1 google.com; do
+        echo "Not yet :3 $(date)"
+        sleep 1
+    done
+
+    rclone mount NAS: NAS --vfs-cache-mode full --vfs-cache-min-free-space 10Gi
+} &> /tmp/boot-nas.txt &
